a92a33eb10e072239de7187a1669639d3c90d7d6
chestnut <chestunttree@.com>
2024-04-02 16:07:37 +0800

    fix: 删除冗余代码

--- src/extension.ts
+++ src/extension.ts
@@ -14,13 +14,8 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('ctools.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ctools!');
-	});
 
-	context.subscriptions.push(disposable, ...createI18nCommand());
+	context.subscriptions.push(...createI18nCommand());
 }
 
 // This method is called when your extension is deactivated
